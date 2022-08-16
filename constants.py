91b5120314dd46ac2bfd30118467ef7b61c7d5e4
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2022-08-16 16:10:03 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,14 +7,14 @@ LEFT_OFFSET, RIGHT_OFFSET = 5, 5
 BOTTOM_OFFSET, TOP_OFFSET = 50, 50
 
 GRAPH_LENGTH, GRAPH_HEIGHT = 40, 35
-DIAGONAL_LENGTH = sqrt(2)
+DIAGONAL_DISTANCE = sqrt(2)
 
 NODE_LENGTH, NODE_HEIGHT = (LENGTH - LEFT_OFFSET - RIGHT_OFFSET) // GRAPH_LENGTH, (HEIGHT - TOP_OFFSET - BOTTOM_OFFSET)\
                                                                                    // GRAPH_HEIGHT
 
 BUTTON_LENGTH, BUTTON_HEIGHT = 120, 32
 
-EMPTY_COLOR = (0, 0, 0)
+EMPTY_COLOR = (0, 0, 0) # Black
 CELL_COLOR = (0, 0, 0)  # Cyan
 WALL_COLOR = (255, 180, 40)  # Gold
 START_COLOR, END_COLOR = (0, 255, 0), (255, 0, 0)  # Green and Red respectively
@@ -24,6 +24,7 @@ BORDER_COLOR = (255, 255, 255)  # White
 BUTTON_COLOR = (255, 0, 0)  # Also Red
 TEXT_COLOR = (255, 255, 255)  # White
 
+PROCESSING_NODE_COLOR = (255, 140, 35) # Orange
 PATH_COLOR = (0, 0, 255)  # Blue
 
 pygame.font.init()
