a933aca899ed9ac0c756fd05d9f13b5a47d55ac9
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2022-08-16 16:09:28 -0700

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -1,8 +1,86 @@
 from sys import setrecursionlimit
-from types import FunctionType
-from graph import Node
+from math import ceil
+from constants import GRAPH_LENGTH, GRAPH_HEIGHT, DIAGONAL_DISTANCE
+from graph import Node, draw_node
 
-def solve(adjacent: FunctionType, graph: list[list[Node]], start_node: Node, end_node: Node):
+def solve(window, graph: list[list[Node]], start_node: Node, end_node: Node):
+    def next_nodes(previous_node: Node, node: Node):
+        # Uses JPS (Jump Point Search), but no jumping because this is just for adjacency,
+        if previous_node is None:
+            nodes = []
+
+            if node.y > 0 and graph[node.y - 1][node.x].type != "wall":
+                nodes.append((graph[node.y - 1][node.x], 1))
+
+            if node.x < GRAPH_LENGTH - 1 and node.y > 0 and graph[node.y - 1][node.x + 1].type != "wall":
+                nodes.append((graph[node.y - 1][node.x + 1], DIAGONAL_DISTANCE))
+
+            if node.x < GRAPH_LENGTH - 1 and graph[node.y][node.x + 1].type != "wall":
+                nodes.append((graph[node.y][node.x + 1], 1))
+
+            if node.x < GRAPH_LENGTH - 1 and node.y < GRAPH_HEIGHT - 1 and graph[node.y + 1][node.x + 1].type != "wall":
+                nodes.append((graph[node.y + 1][node.x + 1], DIAGONAL_DISTANCE))
+
+            if node.y < GRAPH_HEIGHT - 1 and graph[node.y + 1][node.x].type != "wall":
+                nodes.append((graph[node.y + 1][node.x], 1))
+
+            if node.x > 0 and node.y < GRAPH_HEIGHT - 1 and graph[node.y + 1][node.x - 1].type != "wall":
+                nodes.append((graph[node.y + 1][node.x - 1], DIAGONAL_DISTANCE))
+
+            if node.x > 0 and graph[node.y][node.x - 1].type != "wall":
+                nodes.append((graph[node.y][node.x - 1], 1))
+
+            if node.x > 0 and node.y > 0 and graph[node.y - 1][node.x - 1].type != "wall":
+                nodes.append((graph[node.y - 1][node.x - 1], DIAGONAL_DISTANCE))
+
+            return nodes
+
+        previous_x_change, previous_y_change = node.x - previous_node.x, node.y - previous_node.y
+
+        nodes = []
+
+        try:
+            if 0 <= node.y + previous_y_change and 0 <= node.x + previous_x_change and graph[node.y + previous_y_change][node.x + previous_x_change].type != "wall":
+                nodes.append((graph[node.y + previous_y_change][node.x + previous_x_change], 1))
+
+        except IndexError:
+            pass
+
+        if abs(previous_x_change) + abs(previous_y_change) == 1:
+            if abs(previous_x_change) + abs(previous_y_change) == 1:
+                if previous_x_change != 0:
+                    change = previous_x_change
+
+                    if 0 <= node.y - change <= GRAPH_HEIGHT - 1 and 0 <= node.x + change <= GRAPH_LENGTH - 1 and graph[node.y - change][node.x + change].type != "wall":
+                        nodes.append((graph[node.y - change][node.x + change], DIAGONAL_DISTANCE))
+
+                    if 0 <= node.y + change <= GRAPH_HEIGHT - 1 and 0 <= node.x + change <= GRAPH_LENGTH - 1 and graph[node.y + change][node.x + change].type != "wall":
+                        nodes.append((graph[node.y + change][node.x + change], DIAGONAL_DISTANCE))
+
+                else:
+                    change = previous_y_change
+
+                    if 0 <= node.y + change <= GRAPH_HEIGHT - 1 and 0 <= node.x - change <= GRAPH_LENGTH - 1 and graph[node.y + change][node.x - change].type != "wall":
+                        nodes.append((graph[node.y + change][node.x - change], DIAGONAL_DISTANCE))
+
+                    if 0 <= node.y + change <= GRAPH_HEIGHT - 1 and 0 <= node.x + change <= GRAPH_LENGTH - 1 and graph[node.y + change][node.x + change].type != "wall":
+                        nodes.append((graph[node.y + change][node.x + change], DIAGONAL_DISTANCE))
+
+        else:
+            if 0 <= node.x + previous_x_change <= GRAPH_LENGTH - 1 and graph[node.y][node.x + previous_x_change].type != "wall":
+                nodes.append((graph[node.y][node.x + previous_x_change], 1))
+
+            if 0 <= node.y + previous_y_change <= GRAPH_HEIGHT - 1 and graph[node.y + previous_y_change][node.x].type != "wall":
+                nodes.append((graph[node.y + previous_y_change][node.x], 1))
+
+            if 0 <= node.y + previous_y_change <= GRAPH_HEIGHT - 1 and graph[node.y][previous_node.x].type == "wall" and graph[node.y + previous_y_change][previous_node.x].type != "wall":
+                nodes.append((graph[node.y + previous_y_change][previous_node.x], DIAGONAL_DISTANCE))
+
+            if 0 <= node.x + previous_x_change <= GRAPH_HEIGHT - 1 and graph[previous_node.y][node.x].type == "wall" and graph[previous_node.y][node.x + previous_x_change].type != "wall":
+                nodes.append((graph[previous_node.y][node.x + previous_x_change], DIAGONAL_DISTANCE))
+
+        return nodes
+
     setrecursionlimit(2 ** 31 - 1)
 
     minimum_path_per_node = {node: float("inf") for row in graph for node in row}
@@ -11,55 +89,53 @@ def solve(adjacent: FunctionType, graph: list[list[Node]], start_node: Node, end
     best_path = []
 
     def heuristic(node: Node):
-        return abs(end_node.x - node.x) ** 2 + abs(end_node.y - node.y) ** 2
+        # Octile Distance with tie-breaker of 1 + 1/{graph size/2} (super small to avoid over-estimating)
+        x_distance, y_distance = abs(end_node.x - node.x), abs(end_node.y - node.y)
+        return (x_distance + y_distance + (DIAGONAL_DISTANCE - 2) * min(x_distance, y_distance)) * (1 + 1/ceil((GRAPH_LENGTH * GRAPH_HEIGHT)/2))
 
-    def a_star(node: Node, path: set[Node], path_list: list[Node], weight: int = 0):
+    def a_star(previous_node: Node, node: Node, path: set[Node], path_list: list[Node], weight: int = None):
         nonlocal minimum_path_per_node
 
-        if node == end_node:
-            minimum_path_per_node[node] = weight
+        path = path.copy()
+        path.add(node)
 
+        path_list = path_list.copy()
+        path_list.append(node)
+
+        if weight is not None:
+            minimum_path_per_node[node] = min(minimum_path_per_node[node], weight)
+
+        if node == end_node:
             nonlocal best_path
             best_path = path_list.copy()
 
-            return "FOUND"
+            return True
 
-        if weight != 0:
-            path = path.copy()
-            path.add(node)
+        # Comment out the following two lines if you don't want to see the explored nodes that aren't in the path.
+        if weight is not None:
+            draw_node(window, node)
 
-            path_list = path_list.copy()
-            path_list.append(node)
+        if weight is None:
+            weight = 0
 
-            minimum_path_per_node[node] = weight
+        distances = []
 
-        heuristic_blocks = {}
-
-        for adjacent_node, edge_weight in adjacent(node, graph):
-            if adjacent_node in path:
+        for adjacent_node, edge_weight in next_nodes(previous_node, node):
+            if (adjacent_node in path) or (weight + edge_weight >= minimum_path_per_node.get(adjacent_node)):
                 continue
 
             current_heuristic = heuristic(adjacent_node)
+            distances.append((edge_weight, current_heuristic, adjacent_node))
 
-            try:
-                heuristic_blocks[current_heuristic].append((adjacent_node, edge_weight))
+        distances.sort(key=lambda distance: distance[0] + distance[1])
 
-            except KeyError:
-                heuristic_blocks[current_heuristic] = [(adjacent_node, edge_weight)]
+        for edge_weight, _, adjacent_node in distances:
+            found = a_star(node, adjacent_node, path, path_list, weight + edge_weight)
 
-        heuristic_blocks = sorted(heuristic_blocks.items())
+            if found:
+                return True
 
-        for _, nodes in heuristic_blocks:
-            for adjacent_node, edge_weight in nodes:
-                found = False
-
-                if weight + edge_weight < minimum_path_per_node[adjacent_node]:
-                    found = max(found, bool(a_star(adjacent_node, path, path_list, weight + edge_weight)))
-
-                if found:
-                    return "FOUND"
-
-    found = a_star(start_node, set(), [])
+    found = a_star(None, start_node, set(), [])
 
     if found:
         return best_path, minimum_path_per_node[end_node]
